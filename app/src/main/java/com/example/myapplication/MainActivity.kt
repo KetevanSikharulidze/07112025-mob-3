d4894f26994e41d61b439613b360a0a54648a277
student <ketevan.sikharulidze@btu.edu.ge>
2025-11-07 14:06:26 +0400

    initial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,59 @@
+package com.example.myapplication
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.myapplication.databinding.ActivityMainBinding
+import com.example.myapplication.databinding.AnotherLayoutBinding
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var binding : AnotherLayoutBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = AnotherLayoutBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        function()
+//
+//        binding.apply {
+//
+//            NewID
+//
+//
+//        }
+
+    }
+
+    fun anotherFunction() = with(binding){
+        NewID.setOnClickListener {
+
+        }
+    }
+
+
+    private fun function() = with(binding){
+
+        minusBtn.setOnClickListener {
+
+            var amount = amountTV.text.toString().toInt()
+
+            amount--
+
+            amountTV.text = amount.toString()
+        }
+
+        nextBtn.setOnClickListener {
+
+            val price = priceTV.text.toString().toInt()
+
+            val intent = Intent(this@MainActivity, MainActivity2::class.java)
+
+            intent.putExtra("PRICE",price)
+
+            startActivity(intent)
+
+        }
+    }
+}
